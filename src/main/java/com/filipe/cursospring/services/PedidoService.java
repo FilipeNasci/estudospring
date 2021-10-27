2fecbe237ced3fc9af2d825f2ac5e72bbc54b259
Filipe <filipe.n.silva@hotmail.com>
2021-10-27 16:54:05 -0300

    EndPoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/filipe/cursospring/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.filipe.cursospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.filipe.cursospring.domain.Pedido;
+import com.filipe.cursospring.repositories.PedidoRepository;
+import com.filipe.cursospring.services.exceptions.ObjectNotFoundException;
+
+
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) throws ObjectNotFoundException {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
