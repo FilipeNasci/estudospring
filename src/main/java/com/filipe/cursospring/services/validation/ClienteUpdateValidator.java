07fc6daaee32f6b5db18f5dbd1a11870f09dabe8
Filipe <filipe.n.silva@hotmail.com>
2021-10-28 16:53:22 -0300

    Validação customizada de email na atualização de Clientes

---
+++ src/main/java/com/filipe/cursospring/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,51 @@
+package com.filipe.cursospring.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.filipe.cursospring.domain.Cliente;
+import com.filipe.cursospring.dto.ClienteDTO;
+import com.filipe.cursospring.repositories.ClienteRepository;
+import com.filipe.cursospring.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uuid = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null && aux.getId() != uuid) {
+			list.add(new FieldMessage("email", "E-mail já existente."));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage())
+			.addPropertyNode(e.getFieldName()).addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
