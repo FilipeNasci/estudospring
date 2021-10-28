d139490d22e7f04f0f6fb6244f4c1c13060ed93c
Filipe <filipe.n.silva@hotmail.com>
2021-10-28 16:23:24 -0300

    Validação customizada email repetido no insert do Cliente

--- src/main/java/com/filipe/cursospring/services/ClienteService.java
+++ src/main/java/com/filipe/cursospring/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.filipe.cursospring.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -42,6 +44,7 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
--- src/main/java/com/filipe/cursospring/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/filipe/cursospring/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.filipe.cursospring.domain.Cliente;
 import com.filipe.cursospring.domain.enums.TipoCliente;
 import com.filipe.cursospring.dto.ClienteNewDTO;
+import com.filipe.cursospring.repositories.ClienteRepository;
 import com.filipe.cursospring.resources.exceptions.FieldMessage;
 import com.filipe.cursospring.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,7 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
-
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existente."));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage())
